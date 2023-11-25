57c90e57da760fa908f009fb378ea7efdbe797db
nguyen2598 <dinhnguyen250802@gmail.com>
2023-11-25 14:52:50 +0700

    tit

--- modules/handler/index.js
+++ modules/handler/index.js
@@ -1,4 +1,31 @@
 const { Socket } = require('socket.io');
+require('dotenv').config();
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: process.env.GMAIL_NAME,
+      pass: process.env.GMAIL_PASS,
+  },
+});
+
+// Gửi email xác nhận
+function sendVerificationEmail(email, code) {
+  console.log(process.env.GMAIL_NAME, process.env.GMAIL_PASS, email, code);
+  const mailOptions = {
+      from: process.env.GMAIL_NAME,
+      to: email,
+      subject: 'Cảnh báo',
+      html: `Lượng khí gas đang vượt mức vui lòng chú ý`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log('thanh cong');
+      }
+  });
+}
 
 class Handler {
   requestOptions = {
@@ -16,13 +43,14 @@ class Handler {
    */
   emitNewestValue(event, snapshot, socket) {
     const data = snapshot.val();
+    socket.emit('canhbao',`Khí gas đang vượt mức cho phép `)
+        sendVerificationEmail("dinhnguyen110298@gmail.com",dataArray[dataArray.length-1].value )
     let url = `http://localhost:8000/${event}`;
     fetch(url, { ...this.requestOptions, body: JSON.stringify({value: data, time: new Date()}) })
       .then((res) => res.json())
       .then((data) => {
         if (!!data) {
-          console.log("data",data)
-          // socket.emit(event, data);
+          socket.emit(event, data);
         }
       })
       .catch((err) => console.log(err));
